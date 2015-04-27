5306425dfa366c801a8bda798582600c12e1c644
jphager2 <jphager2@gmail.com>
2015-04-27 10:57:05 +0200

    added stylesheets to install generator

--- lib/generators/cal_months/install_generator.rb
+++ lib/generators/cal_months/install_generator.rb
@@ -10,6 +10,18 @@ module CalMonths
         copy_file '../../../config/locales/cal_months_cs.yml', 'config/locales/cal_months_cs.yml'
       end
 
+      def copy_stylesheets
+        copy_file '../../../app/assets/stylesheets/cal_months.css.sass', 'app/assets/stylesheets/cal_months.css.sass'
+        insert_into_file "app/assets/stylesheets/application#{detect_css_format[0]}", "\n#{detect_css_format[1]} require cal_months\n", :after => "require_self"
+      end
+
+      def detect_css_format
+        return ['.css', ' *='] if File.exist?('app/assets/stylesheets/application.css')
+        return ['.css.sass', ' //='] if File.exist?('app/assets/stylesheets/application.css.sass')
+        return ['.sass', ' //='] if File.exist?('app/assets/stylesheets/application.sass')
+        return ['.css.scss', ' //='] if File.exist?('app/assets/stylesheets/application.css.scss')
+        return ['.scss', ' //='] if File.exist?('app/assets/stylesheets/application.scss')
+      end
 
       def copy_views
         copy_file '../../../app/views/cal_months/_calendar.html.haml', 'app/views/cal_months/_calendar.html.haml'
